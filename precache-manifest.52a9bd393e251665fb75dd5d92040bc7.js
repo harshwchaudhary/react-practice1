51950b0a99c7e693b165ba2e10a0940f389ef7e6
harshwardhan <iharshwardhanchaudhary@gmail.com>
2020-09-27 22:40:10 +0530

    Updates

---
+++ precache-manifest.52a9bd393e251665fb75dd5d92040bc7.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd9e4adb00d787fd8b6cbeba763e3332",
+    "url": "/react-practice1/index.html"
+  },
+  {
+    "revision": "f89c1e56b97640109b1d",
+    "url": "/react-practice1/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c2f6cd5e7c43d0d45e12",
+    "url": "/react-practice1/static/css/main.9c803625.chunk.css"
+  },
+  {
+    "revision": "f89c1e56b97640109b1d",
+    "url": "/react-practice1/static/js/2.224c2c8b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-practice1/static/js/2.224c2c8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2f6cd5e7c43d0d45e12",
+    "url": "/react-practice1/static/js/main.271b8932.chunk.js"
+  },
+  {
+    "revision": "93a5d90a639357468c83",
+    "url": "/react-practice1/static/js/runtime-main.a342dc79.js"
+  },
+  {
+    "revision": "29f7a3cf6ab511688ca56a2d7adb5a93",
+    "url": "/react-practice1/static/media/BlackCard.29f7a3cf.png"
+  },
+  {
+    "revision": "261f0e364e4ffb6ed9f18421b442f1ad",
+    "url": "/react-practice1/static/media/BlueCard.261f0e36.png"
+  },
+  {
+    "revision": "803c43600167a502a4a37ba4a56db0d6",
+    "url": "/react-practice1/static/media/PinkCard.803c4360.png"
+  },
+  {
+    "revision": "eda8c9a5c60eb08c92b2e355e1c0e42e",
+    "url": "/react-practice1/static/media/Tick.eda8c9a5.png"
+  },
+  {
+    "revision": "11dca229261a7c0ef7600000d3aed09a",
+    "url": "/react-practice1/static/media/consensys.11dca229.png"
+  },
+  {
+    "revision": "58e36cd3d43446709ab2724a1bb12306",
+    "url": "/react-practice1/static/media/dollar.58e36cd3.png"
+  },
+  {
+    "revision": "872ebce002b1907cb9e20687c8f1cf8a",
+    "url": "/react-practice1/static/media/onju.872ebce0.png"
+  },
+  {
+    "revision": "3f56c6296e06d704fd6fc3215689c239",
+    "url": "/react-practice1/static/media/onjuPhone.3f56c629.png"
+  },
+  {
+    "revision": "c051d9d502ced79fbee045128a8cea63",
+    "url": "/react-practice1/static/media/perc.c051d9d5.svg"
+  },
+  {
+    "revision": "89dd2a43ffb285332c4b44975f015cdc",
+    "url": "/react-practice1/static/media/polychain.89dd2a43.png"
+  }
+]);
